3adf345e3489fdbcab95a41baac08ceb61f16e36
damodaranj <40349259+damodaranj@users.noreply.github.com>
2022-12-23 15:28:43 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,33 @@
+variable "region" {
+  type =string
+  default = "ap-south-1"
+  
+}
+
+variable "subnet_id_1" {
+
+  type = string
+
+  default = "subnet-03bf5faee908d1a2f"
+
+ }
+ variable "subnet_id_2" {
+
+  type = string
+
+  default = "subnet-04acc33d07237005f"
+
+ }
+ variable "subnet_id_3" {
+  
+  type = string
+
+  default = "subnet-0c887fd14fad52374"
+
+ }
+
+ variable "vpc_id" {
+  type = string
+  default = "vpc-0b9f2a5f7910d550b"
+   
+ }
